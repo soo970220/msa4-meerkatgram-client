737edadd899114d5b2f1dea8d74aa2b09d53053e
soo970220 <soo970220@gmail.com>
2026-06-05 11:59:25 +0900

    260605 [dev] 게시글 상세, 삭제아이콘 제어 및 이전 데이터 출력 수정

--- src/page/posts/PostShow.vue
+++ src/page/posts/PostShow.vue
@@ -1,11 +1,14 @@
 <script setup>
-import { onBeforeMount } from 'vue';
+import { onBeforeMount, onBeforeUnmount } from 'vue';
 import { NavigationFailureType, useRoute, useRouter} from'vue-router' ;
 import { usePostShowStore } from '../../store/post/usePostShowStore';
+import { useAuthStore } from '../../store/auth/useAuthStore';
 
 const route = useRoute();
 const router = useRouter();
 const postShowStore = usePostShowStore();
+const authStore = useAuthStore();
+
 
 onBeforeMount(async () => {
   try{
@@ -16,15 +19,20 @@ onBeforeMount(async () => {
    router.replace('/');
   }  
 })
+onBeforeUnmount(postShowStore.clearPostShow);
+
 
 
 </script>
 
 <template>
- <div class="container">
+ <div class="container" v-if="postShowStore.post">
   <div class="image" :style="{backgroundImage:`url(${postShowStore.post?.image})`}"></div>
     <div class="option-box">
-    <div class="delete-icon"></div>
+    <div class="delete-icon"v-if="postShowStore.post.userId === authStore.userInfo.id"
+    ></div>
+    <div v-else></div> 
+    <!-- v-else로 자식을 만들어도되고  div로 delete-icon을 감싸줘도 된다  -->
     <div class="like-box">
       <span>1919</span>
       <div class="like-icon"></div>
--- src/store/post/usePostShowStore.js
+++ src/store/post/usePostShowStore.js
@@ -2,27 +2,33 @@ import { defineStore } from "pinia";
 import myAxios from "../../api/myAxios";
 import { ref } from "vue";
 
-export const usePostShowStore = defineStore('postShowStore',()=>{
- // 1. State
+export const usePostShowStore = defineStore('postShowStore', () => {
+  // 1. State
   const post = ref(null);
 
- // 2. Getter
+  // 2. Getter
 
 
- // 3. Actions
- const getPost = async (id) => {
-  try{
-    const url = `/api/posts/${id}`;
-    const result = await myAxios.get(url);
-    
-    post.value = result.data.data;
-  } catch(error) {
-    throw error;
+  // 3. Actions
+  const getPost = async (id) => {
+    try {
+      const url = `/api/posts/${id}`;
+      const result = await myAxios.get(url);
 
-  }  
- }
+      post.value = result.data.data;
+    } catch (error) {
+      throw error;
+
+    }
+  }
+  const clearPostShow = () => {
+    post.value = null;
+  }
+
+
   return {
-post,
-getPost,
+    clearPostShow,
+    post,
+    getPost,
   }
 });
